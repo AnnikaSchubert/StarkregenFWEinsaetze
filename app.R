e43e17b6aad65d5cf6df0d84335a6182acee3f4b
Bauer.Felix <felix.bauer.2001@web.de>
2023-11-26 11:09:26 +0100

    Adjusted layout of Input-containers Adjusted default map-view Adjusted code for better readabilty

--- app.R
+++ app.R
@@ -37,7 +37,7 @@ ui <- fluidPage(
         display: flex;
         justify-content: space-between;
         align-items: center;
-        padding: 10px 20px;
+        padding: 20px 20px;
         background-color: #f5f5f5;
         border-bottom: 1px solid #ddd;
       }
@@ -45,42 +45,68 @@ ui <- fluidPage(
         display: flex;
         align-items: center;
       }
+      .slider-container,
+      .checkbox-container,
+      .button-container {
+        padding: 10px;
+      }
     "))
   ),
-  div(class = "title-bar",
-      h1("Feuerwehreinsätze im Oberland aufgrund von Starkregen"),
-      div(class = "logo-container",
-          a(href="https://klimaanpassung-oberland.de/",
-            img(src="KARE-Logo-quer-mitClaim-RGB.png",
-                style="width: 15em; height: 5.7em;")
-          )
+  div(
+    class = "title-bar",
+    h1("Feuerwehreinsätze im Oberland aufgrund von Starkregen"),
+    div(
+      class = "logo-container",
+      a(
+        href = "https://klimaanpassung-oberland.de/",
+        img(
+          src = "KARE-Logo-quer-mitClaim-RGB.png",
+          style = "width: 15em; height: 5.7em;"
+        )
       )
+    )
   ),
-  
   fluidRow(
-    column(width = 6, 
-           div(class = "slider-container",
-               sliderInput('date_range', 'Zeitraum auswählen',
-                           min = as.Date("2011-01-01"), max = as.Date("2021-12-31"),
-                           value = c(as.Date("2011-01-01"), as.Date("2021-12-31"))
-               )
-           )
+    column(
+      width = 4,
+      div(
+        class = "slider-container",
+        sliderInput(
+          'date_range', 'Zeitraum auswählen',
+          min = as.Date("2011-01-01"), max = as.Date("2021-12-31"),
+          value = c(as.Date("2011-01-01"), as.Date("2021-12-31"))
+        )
+      )
     ),
-    column(width = 6, align = 'right', style = "margin-top: 30px;",
-           div(class = "button-container",
-               actionButton('show_about', 'Mehr Informationen')
-           )
+    column(
+      width = 4,
+      align = 'center',
+      div(
+        class = "checkbox-container",
+        checkboxGroupInput(
+          'overlap_filter', 'Starkregenereignis als Einsatzgrund',#Titel ändern?
+          choices = list('Option 1' = 1, 'Option 2' = 2, 'Option 3' = 3, 'Option 4' = 4),#Anpassen für korrekte Beschriftung
+          selected = 1:4
+        )
+      )
+    ),
+    column(
+      width = 4,
+      align = 'end',
+      div(
+        class = "button-container",
+        actionButton('show_about', 'Mehr Informationen')
+      )
     )
   ),
-  
   fluidRow(
-    column(width = 6,
-           checkboxGroupInput('overlap_filter', 'Starkregenereignis als Einsatzgrund', choices = 1:4, selected = 1:4)
+    column(
+      width = 12,
+      leaflet::leafletOutput('map', height = 'calc(100vh - 200px)')
     )
-  ),
-  
-  leaflet::leafletOutput('map', height = 'calc(100vh - 200px)')
+  )
 )
+
 
 server <- function(input, output, session) {
   
@@ -88,7 +114,7 @@ server <- function(input, output, session) {
     showModal(modalDialog(HTML("Die Webanwendung basiert auf Einsatzerhebungen der Feuerwehren im Zeitraum von Januar 2011 bis Dezember 2021. Der Datensatz umfasst alle Einsätze im Oberland, die in Zusammenhang mit Unwettern und/oder Überschwemmungen und/oder Wasserschäden stehen. Diese Daten wurden dem KARE-Team freundlicherweise von den Feuerwehren X, X, X und X zur Verfügung gestellt.<br><br> 
                           Die Daten wurden von der LMU München aufbereitet und vom IMK-IFU KIT georeferenziert. Um sicherzustellen, dass Einsätze mit hoher Wahrscheinlichkeit auf Starkregenereignisse zurückzuführen sind, wurden zwei Ansätze gewählt. Zunächst wurden alle Einsatzdaten mit dem Katalog der Starkregenereignissen (CatRaRE) verglichen. Für Einsätze, die weder zeitlich noch räumlich mit den Starkregenereignissen zusammenhingen, wurde zudem eine Medienanalyse in Zeitungen und auf Websites der Feuerwehr durchgeführt, um Einsätze aufgrund anderer Ursachen als Starkregen (z.B. Wasserrohrbruch) auszuschließen.<br><br> 
                           Um die Anonymität der betroffenen Haushalte zu gewährleisten, wurden die Daten verändert. Die GPS-Koordinate zeigt nicht den genauen Standort des betroffenen Gebäudes an, sondern variiert zufällig um etwa 40 m.<br><br>
-                          Sollten Sie weitere Fragen haben, wenden Sie sich bitte an Annika.Schubert@lmu.de."),
+                          Sollten Sie weitere Fragen haben, wenden Sie sich bitte an Annika.Schubert@lmu.de."), #Checkbox-Group erklären
                           footer = tagList(
                             modalButton("Schließen")
                           )))
@@ -109,7 +135,7 @@ server <- function(input, output, session) {
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
-      setView(11.4, 47.8, zoom = 10) %>%
+      setView(11.2, 47.7, zoom = 10) %>%
       addCircleMarkers(
         data = filtered_data(),
         lat = ~lat,
