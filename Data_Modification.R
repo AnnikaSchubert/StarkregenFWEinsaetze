117357325b7b9477f6507ccf43768c103ef3c11a
Bauer.Felix <felix.bauer.2001@web.de>
2023-12-08 10:26:11 +0100

    Created new Excel-Sheet with relevant collumns only

--- Data_Modification.R
+++ Data_Modification.R
@@ -1,3 +1,13 @@
 
 ## Anonymising the Data for the Firebrigade Map
 
+library(dplyr)
+library(openxlsx)
+library(here)
+
+here::here()
+data <- read.xlsx(here('data/Firebrigade_Kopie.xlsx'),sheet = 'Sheet für Tool')
+
+Anonymised_Data <- data %>% select("datum", "lon", "lat", "overlap")
+
+write.xlsx(Anonymised_Data, file = here('data/Anonymised_FirebrigadeData.xlsx'))
