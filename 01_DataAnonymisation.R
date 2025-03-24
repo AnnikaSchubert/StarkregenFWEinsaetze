ae6780fc2287bce8d6d56cb5a070b7a6471d88ba
Annika Schubert <annika.schubert@tutamail.com>
2025-03-24 09:56:06 +0100

    Updates: recoding of overlap variable changed, minor adjustment to the coordinate anonymisation

--- 01_DataAnonymisation.R
+++ 01_DataAnonymisation.R
@@ -6,7 +6,7 @@
 ##                                                  ##
 ##          Annika Schubert & Felix Bauer           ##
 #                                                   ##
-#                    12.12.2023                     ##
+#                    20.01.2025                     ##
 ######################################################
 
 
@@ -17,12 +17,29 @@
 #                                                #
 # ############################################## #
 
+# 1.1 Load Libraries and Data ---------------------------
+
+# 01_DataAnaonymisation
 library(dplyr)
 library(openxlsx)
 library(here)
 
+# 02_App
+library(shiny)
+library(shinythemes)
+library(dplyr)
+library(leaflet)
+library(openxlsx)
+library(shinyBS)
+library(viridis)
+library(here)
 
-# 1.1 Load Data ---------------------------
+
+
+# create reproducible working environment
+renv::init() # initialize renv
+renv::snapshot() # create snapshop of used packages
+
 
 here::here()
 data <- read.xlsx(here('data/raw/Firebrigade_CatRaRe.xlsx'))
@@ -48,35 +65,43 @@ data$overlap[data$overlap == 4 & is.na(data$Media) |
              data$overlap == 4 & data$Media == 0] <- 5
 table(data$overlap)
 
+# recode overlap variable into likelihood variable for better understanding
+data$categ <-  case_when(
+                          data$overlap == 0 ~ 3,     # no overlap = 3) no evidence                   
+                          data$overlap == 1 ~ 1,     # perfect fit = 1) very high prob                
+                          data$overlap == 2 ~ 2,     # date but not area = 2) high         
+                          data$overlap == 3 ~ 2,     # area but not date = 2) high          
+                          data$overlap == 4 ~ 2,     # clustered + media = 2) high            
+                          data$overlap == 5 ~ 3)     # clustered, but no media = 3) no evidence            
 
 
 # 1.3 Operations due to Heavy Precipitation ---------------------------
 
 # keep only fire brigade operations which are classified as very likely
-# due to a heavy precipitation event; validated by
-# - CatRaRE data (overlap 1, 2, 3)
-# - media analysis (overlap 4)
-table(data$overlap, useNA = "always")
-valoperations <- subset(data, overlap > 0 & overlap < 5)
+# or likely due to a heavy precipitation event; validated by
+# CatRaRE data or media analysis
+table(data$categ, useNA = "always")
+valoperations <- subset(data, categ == 1 | categ == 2)
 
 
 # 1.3 Anonymisation ---------------------------
 
-# change lat and lon randomly --> 0.0004 are around 40 meters
-
-# -0.5 adds random noise to the imported coordinates
-# --> Code could be simplified by removing this part and only randomizing through the 0.0004-part I believe
+# add random noise to imported coordinates
+# coordinate in decimal degrees --> change forth forth decimal for ~ 40 m randomisation
+# -  runif(n()): create uniform random numbers [0,1]
+# -  -0.5: substract 0.5 for Nord/South and East/West changes
+# -  * 0.0005 to change coordinate by max. 40 meters (0.0001 = ~7,8m)
 
 set.seed(123)
 
 valoperations <- valoperations %>%
   mutate(
-    lat = lat + (runif(n()) - 0.5) * 0.0004, # Change value to adjust magnitude of randomization --> 0.0004 = 40 meters
-    lon = lon + (runif(n()) - 0.5) * 0.0004)
+    lat = lat + (runif(n()) - 0.5) * 0.0005,  
+    lon = lon + (runif(n()) - 0.5) * 0.0005) 
 
 
 # save only relevant vars
-Anonymised_Data <- valoperations %>% select("datum", "lon", "lat", "overlap")
+Anonymised_Data <- valoperations %>% select("datum", "lon", "lat", "categ")
 
 # save anonymised data for ShinyApp
 write.xlsx(Anonymised_Data, file = here('data/tidy/Anonymised_FirebrigadeData.xlsx'))
